66b6c03e575b35ac6d39b858c10c936393881eea
jakubsmorada <xsmorada@stuba.sk>
2022-03-22 15:44:20 +0100

    first API endpoint, /register

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter
+from app.schemas import *
 
 router = APIRouter(
     prefix="/users",
     tags=["Users"]
 )
+
+
+@router.get("/registration", response_model=UserRegisterOut)
+def penis(registration: UserRegisterIn):
+    return registration
