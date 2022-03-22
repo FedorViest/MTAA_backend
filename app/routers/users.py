9061d860f9d0bf9e0517e579762c909a4357c888
jakubsmorada <xsmorada@stuba.sk>
2022-03-22 19:45:11 +0100

    created /registration API endpoint, sucessfully added user to the db

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends
+
+from app.models import Users
 from app.schemas import *
 from app.database import connect_to_db
 from sqlalchemy.orm import Session
+from app.utils import *
 
 router = APIRouter(
     prefix="/users",
@@ -10,10 +13,16 @@ router = APIRouter(
 
 
 @router.post("/registration", response_model=UserRegisterOut)
-def penis(registration: UserRegisterIn):
-    return registration
+def register(user_credentials: UserRegisterIn, db_conn: Session = Depends(connect_to_db)):
+    hashed_password = pwd_context.hash(user_credentials.password)
+    user_credentials.password = hashed_password
+
+    new_user = Users(**user_credentials.dict())
+
+    db_conn.add(new_user)
+    db_conn.commit()
+    db_conn.refresh(new_user)
+
+    return new_user
 
 
-@router.get("/login", response_model=UserLoginOut)
-def user_login(login_details: UserLoginIn):
-    return login_details
