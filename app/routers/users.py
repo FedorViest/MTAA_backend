56919a7ac2ed609d7ead37b524d18ba74063fa42
jakubsmorada <xsmorada@stuba.sk>
2022-03-23 14:43:29 +0100

    removed /registration API endpoint

--- app/routers/users.py
+++ app/routers/users.py
@@ -12,17 +12,4 @@ router = APIRouter(
 )
 
 
-@router.post("/registration", response_model=UserRegisterOut)
-def register(user_credentials: UserRegisterIn, db_conn: Session = Depends(connect_to_db)):
-    hashed_password = pwd_context.hash(user_credentials.password)
-    user_credentials.password = hashed_password
-
-    new_user = Users(**user_credentials.dict())
-
-    db_conn.add(new_user)
-    db_conn.commit()
-    db_conn.refresh(new_user)
-
-    return new_user
-
 
