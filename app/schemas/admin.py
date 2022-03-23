5fa45908367b72976e86ec4d7a0076254d1f1296
FedorViest <fedo.viest@gmail.com>
2022-03-23 18:07:17 +0100

    Added new schemas to admin.py regarding employees and computers

--- app/schemas/admin.py
+++ app/schemas/admin.py
@@ -1,9 +1,50 @@
 from pydantic import BaseModel
 
+
 class AllRatingsOut(BaseModel):
     id: int
     rating: float
     comment: str
+
+    class Config:
+        orm_mode = True
+
+
+class AddEmployeeIn(BaseModel):
+    name: str
+    password: str
+    email: str
+    position: str
+    skills: str
+
+
+class AddEmployeeOut(BaseModel):
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
+class AddComputerIn(BaseModel):
+    brand: str
+    model: str
+    year_made: str
+
+
+class AddComputerOut(BaseModel):
+    brand: str
+    model: str
+    year_made: str
+
+    class Config:
+        orm_mode = True
+
+
+class GetComputersOut(BaseModel):
+    brand: str
+    model: str
+    year_made: str
 
     class Config:
         orm_mode = True
