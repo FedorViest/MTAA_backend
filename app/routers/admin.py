f4e6a172941c2d2ea6413417dfcb04a2a6e5c659
FedorViest <fedo.viest@gmail.com>
2022-03-23 20:59:26 +0100

    Moved function for checking validity of email to utils.py

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -7,10 +7,7 @@ from app.database import connect_to_db
 from sqlalchemy.orm import Session
 from app.models import *
 from app.utils import pwd_context
-import re
-
-
-email_format_regex = '^[a-zA-z0-9]+[\._]?[a-zA-Z0-9]+[@]\w+[.]\w{2,3}$'
+import app.utils as utils
 
 
 router = APIRouter(
@@ -26,12 +23,6 @@ def all_ratings(db_conn: Session = Depends(connect_to_db)):
     return result
 
 
-def email_valid(email: str):
-    if re.search(email_format_regex, email):
-        return True
-    return False
-
-
 @router.post("/addEmployee", response_model=AddEmployeeOut)
 def post_employee(employee_details: AddEmployeeIn, db_conn: Session = Depends(connect_to_db)):
     hashed_password = pwd_context.hash(employee_details.password)
@@ -39,7 +30,7 @@ def post_employee(employee_details: AddEmployeeIn, db_conn: Session = Depends(co
 
     print(employee_details.email)
 
-    if not email_valid(employee_details.email):
+    if not utils.email_valid(employee_details.email):
         raise HTTPException(status_code=400, detail="Invalid email")
 
     new_user = Users(name=employee_details.name, password=employee_details.password, email=employee_details.email)
--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,13 @@
 from passlib.context import CryptContext
+import re
+
+
+email_format_regex = '^[a-zA-z0-9]+[\._]?[a-zA-Z0-9]+[@]\w+[.]\w{2,3}$'
+
+
+def email_valid(email: str):
+    if re.search(email_format_regex, email):
+        return True
+    return False
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
