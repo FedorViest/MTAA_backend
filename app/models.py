354fa3944f9fed5de402932d980656d9b68b6c8e
jakubsmorada <xsmorada@stuba.sk>
2022-03-25 15:58:41 +0100

    changed Orders and Ratings table, if employee is deleted -> employee_id is set to null

--- app/models.py
+++ app/models.py
@@ -29,7 +29,7 @@ class Ratings(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     customer_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    employee_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    employee_id = Column(Integer, ForeignKey("users.id", ondelete="SET NULL"))
     rating = Column(Float)
     comment = Column(VARCHAR)
 
@@ -39,7 +39,7 @@ class Orders(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     customer_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    employee_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    employee_id = Column(Integer, ForeignKey("users.id", ondelete="SET NULL"))
     pc_id = Column(Integer, ForeignKey("computers.id"), nullable=False)
     date_created = Column(TIMESTAMP(timezone=True), server_default=text('CURRENT_TIMESTAMP'))
     status = Column(VARCHAR(20))
