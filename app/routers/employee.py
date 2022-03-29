2e6b62497b0afc847708eaeaa16934f2a4e862fd
jakubsmorada <xsmorada@stuba.sk>
2022-03-29 18:22:48 +0200

    added summary and descriptions to all the endpoints

--- app/routers/employee.py
+++ app/routers/employee.py
@@ -1,10 +1,7 @@
 from typing import List
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import and_
 from starlette import status
-
-from app.schemas.employee import *
 from app.database import connect_to_db
 from sqlalchemy.orm import Session, aliased
 from app.models import *
@@ -18,9 +15,23 @@ router = APIRouter(
 )
 
 
-@router.put("/updateOrderState/{order_id}", response_model=updateOrderStateOut)
+@router.put("/updateOrderState/{order_id}", response_model=updateOrderStateOut, summary="Update state of the order")
 def update_order(order_info: updateOrderStateIn, order_id: int, db_conn: Session = Depends(connect_to_db),
                  current_user: Users = Depends(oauth.get_user)):
+    """
+    Update state of the specified order with this parameter:
+
+    - **order_id**: id of the order's state to be updated
+
+    Request body has to have this information:
+
+    - **status**: new state of the order
+
+    Response body has to have this information:
+
+    - **status**: new state of the order
+    """
+
     utils.validate_user(current_user, "employee")
 
     query_result = db_conn.query(Orders).filter(Orders.id == order_id)
@@ -34,21 +45,59 @@ def update_order(order_info: updateOrderStateIn, order_id: int, db_conn: Session
     return order_info.dict()
 
 
-@router.get("/getRepairs", response_model=List[getRepairsOut])
+@router.get("/getRepairs", response_model=List[getRepairsOut], summary="Get all current employee' repairs")
 def get_repairs(db_conn: Session = Depends(connect_to_db), current_user: Users = Depends(oauth.get_user)):
+
+    """
+    Get list of all repairs for currently signed in employee
+
+    Response body has to have this information:
+
+    - **id**: order's id
+    - **date_created**: date of order's creation
+    - **status**: state of order
+    - **issue**: computer's issue specified by the customer
+    - **brand**: brand of the computer
+    - **model**: model of the computer
+    - **year_made**: year in which was the computer published
+    - **customer_email**: order's creator (customer) email
+    """
+
     utils.validate_user(current_user, "employee")
 
-    query_result = db_conn.query(Orders).filter(current_user.id == Orders.employee_id).all()
+    user_customer = aliased(Users)
 
-    if not query_result:
+    result = db_conn.query(Orders, Computers, user_customer.email.label("customer_email")). \
+        join(user_customer, user_customer.id == Orders.customer_id). \
+        join(Users, current_user.id == Orders.employee_id).\
+        join(Computers, Orders.pc_id == Computers.id).\
+        all()
+
+    if not result:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="You do not have any repairs")
 
-    return query_result
+    return result
 
 
-@router.get("/getRepairs/{repair_id}", response_model=RepairOut)
+@router.get("/getRepairs/{repair_id}", response_model=RepairOut, summary="Get specific repair")
 def get_repair(repair_id: int, db_conn: Session = Depends(connect_to_db),
                current_user: Users = Depends(oauth.get_user)):
+    """
+    Get specific repair for currently signed in employee
+
+    Required parameters:
+    - **repair_id**: specified order's id
+
+    Response body required information:
+
+    - **id**: order's id
+    - **status**: order's state
+    - **date_created**: order's date of creation
+    - **issue**: computer's issue specified by the customer
+    - **brand**: brand of the computer
+    - **model**: model of the computer
+    - **year_made**: year in which was the computer published
+    """
 
     utils.validate_user(current_user, "employee")
 
--- app/schemas/employee.py
+++ app/schemas/employee.py
@@ -14,12 +14,27 @@ class updateOrderStateOut(BaseModel):
         orm_mode = True
 
 
-class getRepairsOut(BaseModel):
+class orderInfo(BaseModel):
     id: str
-    customer_id: str
-    pc_id: str
+    date_created: datetime.date
     status: str
     issue: str
+
+    class Config:
+        orm_mode = True
+
+class computerInfo(BaseModel):
+    brand: str
+    model: str
+    year_made: int
+
+    class Config:
+        orm_mode = True
+
+class getRepairsOut(BaseModel):
+    Orders: orderInfo
+    Computers: computerInfo
+    customer_email: str
 
     class Config:
         orm_mode = True
